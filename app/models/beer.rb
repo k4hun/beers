688b8549a78d3a151dcb81815ca8ba558e7025fe
k4hun <thameth@gmail.com>
2015-04-06 18:39:26 +0200

    add validations

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,9 @@
 class Beer < ActiveRecord::Base
   belongs_to :style
   belongs_to :brewery
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :style_id, presence: true
+  validates :brewery_id, presence: true
 end
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,3 +1,6 @@
 class Style < ActiveRecord::Base
   has_many :beers
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
 end
+
