d5b84c4da4647a38b4a6239197ba66a45cf3a16b
k4hun <thameth@gmail.com>
2015-04-06 17:04:45 +0200

    add style model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406150227) do
+ActiveRecord::Schema.define(version: 20150406150344) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20150406150227) do
     t.string   "description"
     t.integer  "rating"
     t.integer  "brewery_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "styles", force: true do |t|
+    t.string   "name"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
