ebbd06e7336c37b6d7dc7793b3bde84c9fe6eb98
k4hun <thameth@gmail.com>
2015-04-06 18:46:25 +0200

    managing styles

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -6,18 +6,44 @@ class StylesController < ApplicationController
     @styles = Style.order(:name)
   end
 
-  def new    
+  def new 
+    @style = Style.new   
   end
 
-  def create    
+  def create  
+    @style = Style.new(style_params)  
+    if @style.save
+      redirect_to styles_path, notice: 'Style added!'
+    else
+      render 'new', error: 'Something went wrong!'
+    end
   end
 
-  def edit    
+  def edit
+    current_style    
   end
 
-  def update    
+  def update  
+    @style = current_style
+    if @style.update_attributes(style_params) 
+      redirect_to styles_path, notice: 'Style updated!' 
+    else
+      render 'edit', error: 'Something went wrong!'
+    end
   end
 
-  def destroy    
+  def destroy 
+    current_style.destroy 
+    redirect_to styles_path, notice: 'Style removed!'  
   end
+
+  private
+  def style_params
+    params.require(:style).permit(:name, :description) 
+  end
+
+  def current_style
+    @style ||= Style.find(params[:id])
+  end
+
 end
