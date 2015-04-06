f61c88fb7e118650a8dbc18af76acf7395c50441
k4hun <thameth@gmail.com>
2015-04-06 18:12:56 +0200

    add admin in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Admin.create(email: 'admin@example.com', password: '12345678')
+
 10.times do
   Style.create(name: Faker::Lorem.word, description: Faker::Lorem.sentence)
 end
