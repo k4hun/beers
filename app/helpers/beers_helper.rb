0610481b8dfc08b9390e5f0066dc7c0c19b94869
k4hun <thameth@gmail.com>
2015-04-07 12:04:47 +0200

    fix rating star helper method

--- app/helpers/beers_helper.rb
+++ app/helpers/beers_helper.rb
@@ -1,5 +1,2 @@
 module BeersHelper
-  def short_desc(desc)
-    desc.truncate(30, omission: '...')
-  end
 end
--- app/helpers/public_helper.rb
+++ app/helpers/public_helper.rb
@@ -1,2 +1,13 @@
 module PublicHelper
+  def short_desc(desc)
+    desc.truncate(30, omission: '...')
+  end
+
+  def rating_stars(rating)
+    stars = []
+    rating.times do
+      stars << image_tag('rate_star.png', size: "20x20")
+    end
+    stars
+  end
 end
