ebf154a74519da0bf8e63b4a1454340875892078
k4hun <thameth@gmail.com>
2015-04-14 16:36:58 +0200

    add some styling to main page

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,6 @@
 class PublicController < ApplicationController
   def index
     @beers = Beer.order(:name).paginate(page: params[:page]).search(params)
+    @styles = Style.all
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def side_styles
+    Style.order(:name)
+  end
+
+  def recently_commented
+    Style.order(:name).limit(7)
+  end
 end
